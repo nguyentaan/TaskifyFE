2c446f29c36adde92469f2c3211057e5a179b27d
nguyentaan <vantaan2002@gmail.com>
2024-10-31 16:34:32 +0700

    get and create task

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../models/task.model';
+import { error } from 'console';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -8,26 +11,64 @@ import { AuthService } from '../../services/auth.service';
 export class DashboardComponent implements OnInit {
   userData: any;
   isAddTaskVisible = false;
-  newTask = { title: '', description: '', dueDate: '' };
+  task: Task | undefined;
+  newTask = { title: '', description: '', dueDate: '', userId: '' };
+  tasks: Task[] = [];
+
+  constructor(
+    private authService: AuthService,
+    private taskService: TaskService
+  ) {}
+
+  ngOnInit(): void {
+    this.userData = this.authService.getUserDataFromToken();
+    this.newTask.userId = this.userData.sub;
+    this.getTaskById(this.userData.sub);
+    // console.log(this.userData);
+    // console.log(this.userData.sub);
+  }
 
   toggleAddTask(): void {
     this.isAddTaskVisible = !this.isAddTaskVisible;
   }
 
+  getTaskById(userId: string): void {
+    this.taskService.getTaskById(userId).subscribe(
+      (response: Task[]) => {
+        this.tasks = response;
+        console.log('Task:', this.tasks);
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
+
   createTask(): void {
+    const taskToCreate: Task = {
+      ...this.newTask,
+      dueDate: new Date(this.newTask.dueDate), // Convert to Date type
+    };
     // Logic to create a new task (e.g., send to a service)
-    console.log('New Task Created:', this.newTask);
-    // Reset the form after submission
-    this.newTask = { title: '', description: '', dueDate: '' };
-    this.isAddTaskVisible = false; // Hide the form after creating the task
+    this.taskService.createTask(taskToCreate).subscribe(
+      (createdTask: Task) => {
+        console.log('New Task Created:', createdTask);
+
+        this.newTask = {
+          title: '',
+          description: '',
+          dueDate: '',
+          userId: this.userData.sub,
+        };
+        this.isAddTaskVisible = false;
+        this.getTaskById(this.userData.sub);
+      },
+      (error: any) => {
+        console.error('Error creating task:', error);
+      }
+    );
   }
 
-  constructor(private authService: AuthService) {}
-
-  ngOnInit(): void {
-    this.userData = this.authService.getUserDataFromToken();
-    console.log(this.userData);
-  }
   logout(): void {
     this.authService.logout();
   }
